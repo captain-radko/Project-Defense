2e6b0081da82dc22b8b1e269d1f914f7dcb008db
Radoslav Radoev <rakoki8@gmail.com>
2018-11-29 18:32:17 +0200

    Added services + EmailSender

---
+++ src/Services/IdeGames.Services/EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using IdeGames.Services.Contracts;
+
+namespace IdeGames.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
