d977c4074b46a7cff73ddaed831a1cf08768966d
Radoslav Radoev <rakoki8@gmail.com>
2018-12-09 17:29:02 +0200

    Added AdminIndex page

---
+++ src/Web/IdeGames.Web/Controllers/AdministrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdeGames.Web.Controllers
+{
+    public class AdministrationController : BaseController
+    {
+        public IActionResult AdminIndex()
+        {
+            return this.View();
+        }
+    }
+}
